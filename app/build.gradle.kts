f71a075da8583e3b61e177592ac8f510681c6d8e
chuanting.xu <ctrl.xu@foxmail.com>
2025-03-14 17:38:51 +0800

    modify app name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.nevermore.demo"
+        applicationId = "com.nevermore.android"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
